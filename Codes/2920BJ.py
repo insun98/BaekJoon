a24e4d087d234b696ea0d1b0e7c08f1966c86711
insun98 <21900349@handong.edy>
2022-07-27 22:27:25 +0900

    update

--- Codes/2920BJ.py
+++
@@ -1,9 +0,0 @@
-scale = input()
-scale = scale.replace(" ", "")
-ascending = "12345678"
-descending = "87654321"
-if scale == ascending:
-    print("ascending")
-elif scale == descending:
-    print("descending")
-else : print("mixed")
